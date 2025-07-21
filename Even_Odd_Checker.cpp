35bfafc9d4192f6cb6a6b4109a48c41dec7562d5
Advika Bhosle <advikabhosle@gmail.com>
2025-07-21 22:51:33 +0530

    Decision Making Statements

---
+++ Even_Odd_Checker.cpp
@@ -0,0 +1,16 @@
+// Name - Advika Bhosle 
+// PRN - 24070123007
+// Batch - EnTC - A1 
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int num1;
+    cout << "Please enter first number : ";
+    cin >> num1;
+    
+    if(num1 % 2 == 0) cout << "The number "<< num1 << " is even";
+    else cout<< "The number "<< num1 << " is odd";
+    return 0;
+}
